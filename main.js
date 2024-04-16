5b24f8b5ef33b774476c3ba12986cb71443f60d3
WafaaKanwal <wafa.kanwal.khi@gmail.com>
2024-04-17 01:39:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+// import the 'inquirer' module, which is a command line interface for Node.js
+import inquirer from "inquirer";
+//import chalk
+import chalk from "chalk";
+// Declare a  'answers' & assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: chalk.yellowBright("Enter your sentence to count the word: ")
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(chalk.cyanBright(`Your sentence word count is "${chalk.yellowBright(words.length)}".`));
